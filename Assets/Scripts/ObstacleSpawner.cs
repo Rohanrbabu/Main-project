9bb42dcf5d4ec36e0cbe83b45a78d021ec2dc3c7
Rohanrbabu <rohanrbabu6@gmail.com>
2026-01-10 17:16:16 +0530

    120. Obstacle prefab variant

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstacleprefab;
+    [SerializeField] GameObject[] obstacleprefabs;
     [SerializeField] float st = 1f;
+    [SerializeField] Transform obstacleparent;
+    [SerializeField] float spawnwidth=4f;
     void Start()
     {
         StartCoroutine(Spawnobstacleroutine());
@@ -14,8 +17,10 @@ public class ObstacleSpawner : MonoBehaviour
     {
          while(true)
         {
+            GameObject obstacleprefab=obstacleprefabs[Random.Range(0,obstacleprefabs.Length)];
+            Vector3 spawnposition= new Vector3(Random.Range(-spawnwidth,spawnwidth),transform.position.y,transform.position.z);
             yield return new WaitForSeconds(st);
-            Instantiate(obstacleprefab, transform.position, Random.rotation);
+            Instantiate(obstacleprefab, transform.position, Random.rotation,obstacleparent);
             
         }
     }
